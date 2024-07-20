40dbecba483bb282a7b96ee45655fcb841d28039
Lenovo <bhargavpatel6832@gmail.com>
2024-07-20 22:58:47 +0530

    add to cart and footer working on

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,16 +10,18 @@ const FoodDisplay = ({ category }) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item.id}
-              name={item.name}
-              des={item.description}
-              image={item.image}
-              price={item.price}
-            />
-          );
+          if ((category === "All" || category === item.category)) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                des={item.description}
+                image={item.image}
+                price={item.price}
+              />
+            );
+          }
         })}
       </div>
     </div>
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,19 +1,38 @@
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = (itemId) => {
+        setCartItem((prev) => ({
+            ...prev,
+            [itemId]: (prev[itemId] || 0) + 1
+        }));
+    };
+
+    const removeFromCart = (itemId) => {
+        setCartItem((prev) => ({
+            ...prev,
+            [itemId]: Math.max((prev[itemId] || 0) - 1, 0)
+        }));
+    };
 
     const contextvalue = {
-        food_list
-    }
+        food_list,
+        cartItem,
+        setCartItem,
+        addToCart,
+        removeFromCart
+    };
+    
     return (
         <StoreContext.Provider value={contextvalue}>
             {props.children}
         </StoreContext.Provider>
-    )
-
-}
+    );
+};
 
 export default StoreContextProvider;

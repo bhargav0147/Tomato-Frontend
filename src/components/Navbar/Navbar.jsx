ceae7e2c26d6fb98f3a1de24cf56148cee165d96
Lenovo <bhargavpatel6832@gmail.com>
2024-07-21 18:10:31 +0530

    finish home and responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [menu, setmenu] = useState("home");
@@ -8,10 +9,10 @@ const Navbar = () => {
         <div className="navbar">
             <img src={assets.logo} alt="" className="logo" />
             <ul className="navbar-menu">
-                <li onClick={() => setmenu("home")} className={menu === 'home' ? 'active' : ''}>Home</li>
-                <li onClick={() => setmenu("menu")} className={menu === 'menu' ? 'active' : ''}>Menu</li>
-                <li onClick={() => setmenu("mobileapp")} className={menu === 'mobileapp' ? 'active' : ''}>Mobile App</li>
-                <li onClick={() => setmenu("contactus")} className={menu === 'contactus' ? 'active' : ''}>Contact Us</li>
+                <Link to='/' onClick={() => setmenu("home")} className={menu === 'home' ? 'active' : ''}>Home</Link>
+                <a href="#explore-menu" onClick={() => setmenu("menu")} className={menu === 'menu' ? 'active' : ''}>Menu</a>
+                <a href="#app-download" onClick={() => setmenu("mobileapp")} className={menu === 'mobileapp' ? 'active' : ''}>Mobile App</a>
+                <a href="#footer" onClick={() => setmenu("contactus")} className={menu === 'contactus' ? 'active' : ''}>Contact Us</a>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
